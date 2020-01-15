b49776f1615a5d3d7ea20831e130fedbe89cb90f
shubham100ev <shubhamsingh19981110@gmail.com>
2020-01-16 00:55:35 +0530

    Font and Theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:help_me/routes/intro_screen.dart';
 
+var greyColor = Color(0xFF707070);
+
 void main() => runApp(new MaterialApp(
       theme: ThemeData(
+          fontFamily: 'Product Sans',
           brightness: Brightness.light,
           textTheme: TextTheme(
-              headline: TextStyle(fontSize: 32, color: Color(0xFF2FB056)))),
-
+              headline: TextStyle(
+                  fontSize: 32,
+                  color: Color(0xFF2FB056),
+                  fontWeight: FontWeight.w600),
+              title: TextStyle(
+                  fontSize: 28,
+                  color: greyColor,
+                  fontWeight: FontWeight.w600),
+              body1: TextStyle(
+                  fontSize: 20,
+                  color: greyColor,
+                  fontWeight: FontWeight.w600),
+              body2: TextStyle(
+                  fontSize: 16,
+                  color: greyColor,
+                  fontWeight: FontWeight.w600))),
       home: IntroScreen(),
     ));
--- lib/routes/intro_screen.dart
+++ lib/routes/intro_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:help_me/main.dart';
 import 'package:help_me/routes/login_screen.dart';
 
 class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
     );
   }
 }
