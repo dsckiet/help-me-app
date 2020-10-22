68f44ac0a371700c2bc837bdeae23a57dc11d92a
Ayush783 <ayushc45xxx@gmail.com>
2020-10-23 01:22:00 +0530

    _

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:help_me/injection.dart';
-import 'package:help_me/ui/screens/home_screens/home_screen.dart';
-import 'package:help_me/ui/shared/theme_data.dart';
+import 'package:help_me/presentation/core/app_widget.dart';
 import 'package:injectable/injectable.dart';
 
 void main() {
   //WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod);
-  runApp(
-    (
-        //AppWidget()
-        MaterialApp(
-      theme: kThemeData,
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    )),
-  );
+  runApp((AppWidget()));
+  //       MaterialApp(
+  //     theme: kThemeData,
+  //     debugShowCheckedModeBanner: false,
+  //     home: HomeScreen(),
+  //   )),
+  // );
 }
