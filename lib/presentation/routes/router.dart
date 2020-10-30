4ed1fb81bc3beb12015bb1f8ef980f27b4d5956e
Ayush783 <ayushc45xxx@gmail.com>
2020-10-30 20:49:37 +0530

    re did dependency version solving

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,12 +1,32 @@
-// import 'package:auto_route/auto_route_annotations.dart';
-// import 'package:help_me/pages/initial_page.dart';
-// import 'package:help_me/pages/second_page.dart';
-// import 'package:help_me/pages/third_page.dart';
+import 'package:auto_route/auto_route_annotations.dart';
+import 'package:help_me/pages/initial_page.dart';
+import 'package:help_me/pages/second_page.dart';
+import 'package:help_me/pages/third_page.dart';
+import 'package:help_me/presentation/auth/google_sign_in_page/intro_screen.dart';
+import 'package:help_me/presentation/auth/login_form/login_form_screen.dart';
+import 'package:help_me/presentation/auth/register_form/register_form_screen.dart';
+import 'package:help_me/presentation/first_aid/first_aid_content_screen.dart';
+import 'package:help_me/presentation/first_aid/first_aid_screen.dart';
+import 'package:help_me/presentation/help_alert/help_screen.dart';
+import 'package:help_me/presentation/home_screen/home_screen.dart';
+import 'package:help_me/presentation/nearby_healthcare_centres/nearby_hcc.dart';
+import 'package:help_me/presentation/precautions/precaution_content_page.dart';
+import 'package:help_me/presentation/precautions/precaution_screen.dart';
+import 'package:help_me/presentation/share_location/share_location_screen.dart';
+import 'package:help_me/presentation/user_profile/profile_screen.dart';
 
-// @autoRouter
-// class $Router {
-//   @initial
-//   InitialPage initialPage;
-//   SecondPage secondPage;
-//   ThirdPage thirdPage;
-// }
+@MaterialAutoRouter(routes: <AutoRoute>[
+  MaterialRoute(page: IntroScreen, initial: true),
+  MaterialRoute(page: LoginScreen),
+  MaterialRoute(page: RegisterScreen),
+  MaterialRoute(page: HomeScreen),
+  MaterialRoute(page: ProfileScreen),
+  MaterialRoute(page: HelpScreen),
+  MaterialRoute(page: ShareLocationScreen),
+  MaterialRoute(page: NearbyHcc),
+  MaterialRoute(page: PrecautionScreen),
+  MaterialRoute(page: PrecautionsContent),
+  MaterialRoute(page: FirstAidScreen),
+  MaterialRoute(page: FirstAidContent),
+])
+class $Router {}
